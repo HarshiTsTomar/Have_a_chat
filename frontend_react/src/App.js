f776d61b31d6805d8599a7cf9de0472c38440107
Harshit <harshitt59@gmail.com>
2023-04-17 11:11:27 +0530

    first_commit

---
+++ frontend_react/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await axios.post('/api/answer', { question: question });
+    setAnswer(response.data.answer);
+  };
+
+  return (
+    <div>
+      <h1>Question-Answering System</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Question:
+          <input type="text" value={question} onChange={(event) => setQuestion(event.target.value)} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <strong>Answer:</strong> {answer}
+      </div>
+    </div>
+  );
+}
+
+export default App;

29ea15e5862dfbd89ea2b0e8f7a911d3c18afcb8
Harshit <harshitt59@gmail.com>
2023-04-19 09:35:07 +0530

    added tailwind

--- frontend_react/tailwind.config.js
+++ frontend_react/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
